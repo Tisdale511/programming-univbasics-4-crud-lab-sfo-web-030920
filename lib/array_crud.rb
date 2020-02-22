f61c38feaa9abe7f30a37e8324ea79cabd932dff
Tisdale Fry <tisdalefry@gmail.com>
2020-02-22 00:03:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr = ["elf", "butt", 9, 3]
+  arr.push('ele')
+  return arr
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  arr = ["elf", "butt", 9, 3]
+  arr.unshift('ele')
+  return arr
 end
 
 def remove_element_from_end_of_array(array)
